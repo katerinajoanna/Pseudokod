d525a0ebaf1a43284bf82390f49e47a1d2d3bb93
katerinajoanna <kasiapotar@gmail.com>
2023-12-20 09:54:00 +0100

    Uppgift låst

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // G-Version - Split the Nota
 // Skriv din pseudokod innanför nedanstående kommentarsblock
 /*
-Fumktion dela räkningen(summa, antal vänner, dricks)
+Funktion dela räkningen(summa, antal vänner, dricks)
     Set variabel summa_med_tips = summa * (1 + dricks)
     summa_per_person = summa_med_tips / antal_vänner
     returnera summa_per_person
@@ -28,3 +28,46 @@ function isOneLetterApart(wordOne, wordTwo)
 end function
 
 */
+
+/* Här nedan är min pseudokod */
+
+/*
+
+funktion play()
+    SET variabel ordbok = ["...", "...", "...", "...", "...", "...", "...", "..."];
+    SET variabel startOrd till "FOUR";
+    SET variabel slutOrd till "FIVE";
+   
+    
+
+     while (loopen kör tills startOrd lika med slutOrd) 
+        SET variabel nyttord till input från användare; 
+        if(startOrd och nyttord har lika många bokstäver); 
+            if (om isoneletterapart är true OCH om ordbok innehåller nyttord)
+                startOrd tilldelas nyttord;
+            end if
+         end if
+        else
+            Skriv ut "De ord i det engelka språket måste innehålla lika många bokstäver!"
+        end else
+    end while
+    Skriv ut grattiss du har vunnit!
+end function
+
+// Deklarera function isOneLetterApart
+
+function isOneLetterApart(FOUR, FIVE) 
+    SET variabel isoneletterapart lika till 0;    
+    
+    for (för varje bokstav i variabeln FOUR)
+        if om bokstaven på plats index i FOUR är lika med plats index i FIVE;
+            isoneletterapart++;
+        end if
+    end for     
+            
+    returnerar variabeln diffCount lika med 1;
+
+end fuktion
+    
+*/
+
